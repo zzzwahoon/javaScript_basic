e204a35357defc496ae2c5b6c647888ed9feb96b
ZzzwaHoon <kkjw94@gmail.com>
2024-04-29 17:54:10 +0900

    01-02 - JS 데이터 - 원시형 - String

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log(1234)
+// 문자(string)은 따옴표를 사용합니다.
+const s1 = 'Neo'
+const s2 = '123'
+
+// 템플릿 리터럴(Literal)은 백틱을 사용하며, ${} 안에 데이터를 보간(Interpolation)합니다.
+const s3 = `My name is ${s1}, I'm ${s2}.`
+
+console.log(s3)
