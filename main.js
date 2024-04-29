2d2fa8627ddbb2d6caba53a690b35b6648a20ef2
ZzzwaHoon <kkjw94@gmail.com>
2024-04-29 17:57:07 +0900

    01-02 - JS 데이터 - 원시형 - Number

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// string
 // 문자(string)은 따옴표를 사용합니다.
 const s1 = 'Neo'
 const s2 = '123'
@@ -6,3 +7,17 @@ const s2 = '123'
 const s3 = `My name is ${s1}, I'm ${s2}.`
 
 console.log(s3)
+
+// number
+// 숫자(number)은 정수 및 부동소수점 숫자(floating point number)를 나타냅니다.
+const n1 = 123
+const n2 = 12.345
+
+// + - * /
+
+// 숫자가 아닌 숫자는 NaN(Not a Number)으로 표시됩니다.
+const n3 = n1 + 'abc'
+const n4 = n1 + undefined // undefined : 정의 되지 않았다
+
+console.log(n3) // '123abc'
+console.log(n4) // NaN
