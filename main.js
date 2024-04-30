5d1d369f95178cdb3d350171a46016e15c2356e5
ZzzwaHoon <kkjw94@gmail.com>
2024-04-30 16:25:06 +0900

    01-07 - JS 데이터 - 변수(const, let)

--- main.js
+++ main.js
@@ -176,3 +176,20 @@ const h1El_2 = document.querySelector('h1')
 if (h1El_2) { 
   console.log(h1El_2.textContent)
 }
+
+// 변수(Variable)란, 데이터(값)의 이름을 지정한 것입니다.
+// 이름이 있으면, 그 이름으로 언제든지 데이터를 재사용할 수 있습니다.
+
+// const 키워드는 상수(Constant)를 의미하며, 한 번 선언하면 다른 값으로 바꿀 수 없습니다.
+const c = 12
+console.log(c)
+console.log(c)
+// c = 34 // Error: Assignment to constant variable.
+
+// let 키워드는 선언한 값을 다른 값으로 바꿀 수 있습니다.
+let l = 56
+console.log(l)
+console.log(l)
+l = 78
+console.log(l)
+console.log(l)
