3c5961732b45b849ad9ce6934e551a47e40d708f
ZzzwaHoon <kkjw94@gmail.com>
2024-04-30 16:04:48 +0900

    01-05 - JS 데이터 - 참조형 - Object

--- main.js
+++ main.js
@@ -90,3 +90,29 @@ console.log(user['age'])
 console.log(user['isValid'])
 console.log(user['emails'])
 console.log(user['hello']())
+
+// 함수(Function)란, 어떤 작업을 수행하기 위해 필요한 여러 코드의 집합으로,
+// 코드를 추상화하고 재사용성을 확보합니다.
+// 이 함수를 자바스크립트에서는 하나의 데이터 종류로 취급합니다.
+
+// 함수 선언문(Declaration)
+function add(a, b) {
+  // console.log(a)
+  // console.log(b)
+  return a + b
+}
+
+console.log(add)
+console.log(add(1, 3))
+console.log(add(4, 12))
+console.log(add(5, 7))
+
+// 함수 표현식(Expression)
+const sub = function (a, b) {
+  return a - b
+}
+
+console.log(sub)
+console.log(sub(1, 3))
+console.log(sub(4, 12))
+console.log(sub(5, 7))
