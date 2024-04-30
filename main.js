12bd12c0956748ca4a105029c60737d8b13e6371
ZzzwaHoon <kkjw94@gmail.com>
2024-04-30 16:12:02 +0900

    01-04 - JS 데이터 - 참조형 - Array

--- main.js
+++ main.js
@@ -116,3 +116,28 @@ console.log(sub)
 console.log(sub(1, 3))
 console.log(sub(4, 12))
 console.log(sub(5, 7))
+
+// 배열(Array) 데이터는 순서가 있는 여러 데이터의 집합입니다.
+// 배열에 포함된 각 데이터는 아이템(Item) 혹은 요소(Element)라고 부릅니다.
+const fruits = ['Apple', 'Banana', 'Cherry']
+const numbers = [12, 27, 5, 9, 103]
+
+console.log(fruits)
+console.log(numbers)
+
+// 배열의 길이를 확인합니다. (갯수확인)
+console.log(fruits.length)
+console.log(numbers.length)
+
+// 배열의 아이템 번호(Index)로 아이템을 확인합니다.
+// 숫자는 0부터 시작합니다. (Zero-based Numbering)
+console.log(fruits[2])
+console.log(numbers[4])
+
+// 배열의 모든 아이템을 순회하며 확인합니다.
+for (let i = 0; i < fruits.length; i += 1) {
+  console.log(fruits[i])
+}
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i])
+}
