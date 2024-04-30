9d3c53f7e7c673cdb81732b30f09696020cf8157
ZzzwaHoon <kkjw94@gmail.com>
2024-04-30 15:52:06 +0900

    01-05 - JS 데이터 - 참조형 - Object

--- main.js
+++ main.js
@@ -59,3 +59,34 @@ function fn() {
   // return 123
 }
 console.log(fn())
+
+// 객체(Object) 데이터는 순서가 없는 Key(키)와 Value(값)의 쌍으로 이루어진 데이터 집합입니다.
+// 객체에 포함된 각 데이터를 속성(Property)라고 부르고,
+// 만약 그 데이터가 함수인 경우에는, 메소드(Method)라고 부릅니다.
+const user = {
+  // key: value
+  name: 'Neo',
+  age: 85,
+  isValid: true,
+  emails: ['neo@abc.com', 'neo@xyz.com'],
+  hello: function () {
+    return `My name is ${this.name}, I'm ${this.age}!`
+  }
+}
+console.log(user)
+
+// 점 표기법(Dot Notation)을 사용해, 객체의 속성이나 메소드에 접근할 수 있습니다.
+console.log(user.name)
+console.log(user.age)
+console.log(user.isValid)
+console.log(user.emails)
+console.log(user.hello())
+
+const key = 'name';
+// 대괄호 표기법(Bracket Notation)을 사용해, 객체의 속성이나 메소드에 접근할 수 있습니다.
+console.log(user[key])
+console.log(user['name'])
+console.log(user['age'])
+console.log(user['isValid'])
+console.log(user['emails'])
+console.log(user['hello']())
