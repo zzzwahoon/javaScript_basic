f9b121243b7505e3c524def3473ea934a2cdd792
ZzzwaHoon <kkjw94@gmail.com>
2024-04-30 16:19:14 +0900

    01-09 - JS 데이터 - 참과 거짓 (Truthy & Falsy)

--- main.js
+++ main.js
@@ -141,3 +141,38 @@ for (let i = 0; i < fruits.length; i += 1) {
 for (let i = 0; i < numbers.length; i += 1) {
   console.log(numbers[i])
 }
+
+// '참'으로 평가되는 값(Truthy)
+if (true) { console.log('참!') }
+if ({}) { console.log('참!') }
+if ([]) { console.log('참!') }
+if (42) { console.log('참!') }
+if ('0') { console.log('참!') }
+if ('false') { console.log('참!') }
+if (new Date()) { console.log('참!') }
+if (-42) { console.log('참!') }
+if (12n) { console.log('참!') }
+if (3.14) { console.log('참!') }
+if (-3.14) { console.log('참!') }
+if (Infinity) { console.log('참!') }
+if (-Infinity) { console.log('참!') }
+
+// '거짓'으로 평가되는 값(Falsy)
+if (false) { console.log('거짓..') }
+if (null) { console.log('거짓..') }
+if (undefined) { console.log('거짓..') }
+if (0) { console.log('거짓..') }
+if (-0) { console.log('거짓..') }
+if (NaN) { console.log('거짓..') }
+if (0n) { console.log('거짓..') }
+if ('') { console.log('거짓..') }
+
+const fruits_2 = ['Apple', 'Banana']
+if (fruits_2.length) {
+  console.log('과일이 있어요!')
+}
+
+const h1El_2 = document.querySelector('h1')
+if (h1El_2) { 
+  console.log(h1El_2.textContent)
+}
