603e5f7d95b6a1cdef27710590f6c73b07559c3e
ZzzwaHoon <kkjw94@gmail.com>
2024-04-30 17:05:01 +0900

    02-02 - 연산자와 구문 - 부정, 비교 연산자

--- main.js
+++ main.js
@@ -320,3 +320,55 @@ e += 1
 console.log(e)
 e -= 1
 console.log(e)
+
+// ** 부정, 비교 연산자 **
+
+// 부정 연산자(Negation Operator)는 참과 거짓의 반댓값을 불린 데이터로 변환합니다.
+
+console.log(!true) // true
+console.log(!false) // true
+
+console.log('중첩 사용 ============')
+console.log(!0) // true
+console.log(!!0)  // false
+console.log(!!!0)  // true
+console.log(!!!!0)  // false
+
+console.log('거짓(Falsy) ============')
+console.log(!null) // true
+console.log(!NaN) // true
+console.log(!undefined) // true
+console.log(!'') // true
+
+console.log('참(Truthy) =============')
+console.log(!{})  // true
+console.log(![])  // false
+console.log(!'A') // false
+
+// 비교 연산자(Comparison Operators)는 두 데이터를 비교할 때 사용합니다.
+const a_5 = 1
+const b_5 = 3
+
+// 동등(형 변환!)
+console.log(a_5 == b_5) // false
+
+// 부등(형 변환!)
+console.log(a_5 != b_5) // true
+
+// 일치
+console.log(a_5 === b_5) // false
+
+// 불일치
+console.log(a_5 !== b_5) // true
+
+// 큼
+console.log(a_5 > b_5) // false
+
+// 크거나 같음
+console.log(a_5 >= b_5) // false
+
+// 작음
+console.log(a_5 < b_5) // true
+
+// 작거나 같음
+console.log(a_5 <= b_5) // true
