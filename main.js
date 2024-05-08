b01332b2426532d04b5fe3fe6e791d44d4a96da4
ZzzwaHoon <kkjw94@gmail.com>
2024-05-08 15:46:46 +0900

    04-04 - 표준 내장 객체 - Math

--- main.js
+++ main.js
@@ -1067,3 +1067,52 @@ console.log(Number.isNaN(NaN))
 console.log(Number.isNaN(123))
 console.log(Number.isNaN(Infinity))
 console.log(Number.isNaN('abc'))
+
+// 04-04 - 표준 내장 객체 - Math
+console.log('** 04-04 - 표준 내장 객체 - Math **')
+
+// Math(숫자) 표준 내장 객체(Built-in Object)
+// Math.abs() - 주어진 숫자의 절댓값을 반환한다
+
+console.log(Math.abs(7))
+console.log(Math.abs(-7))
+console.log(Math.abs(3.14))
+console.log(Math.abs(-3.14))
+
+// Math.ceil() - 주어진 숫자를 올림해 정수를 반환
+// Math.floor() - 주어진 숫자를 내림해 정수를 반환
+// Math.round() - 주어진 숫자를 반올림해 정수를 반환
+console.log(Math.ceil(3.1415926535))  // 4
+console.log(Math.ceil(3.1415926535 * 100) / 100)  // 3.15
+console.log(Math.floor(3.1415926535))  // 3
+console.log(Math.floor(3.1415926535 * 100) / 100)  // 3.14
+
+console.log(Math.round(3.141))  // 3
+console.log(Math.round(3.768))  // 4
+
+console.log(Math.round(3.141 * 10) / 10) // 3.1
+console.log(Math.round(3.768 * 10) / 10) // 3.8
+
+// Math.max() - 주어진 숫자 중 가장 큰 숫자를 반환
+// Math.min() - 주어진 숫자 중 가장 작은 숫자를 반환
+console.log(Math.max(10, 128, 12, 49, 7)) // 128
+console.log(Math.min(10, 128, 12, 49, 7)) // 7
+
+const numbers_3 = [10, 128, 12, 49, 7]
+console.log(Math.max(...numbers_3)) // 128
+console.log(Math.min(...numbers_3)) // 7
+
+// Math.random() - '0' 이상 '1' 미만의 난수(무작위 수)를 반환
+console.log(Math.random())
+console.log(Math.random())
+
+// 원하는 범위의 무작위 정수 얻는 함수
+function random(min = 0, max = 10) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+console.log(random()) // 0 ~ 9
+console.log(random(0, 100)) // 0 ~ 99
+console.log(random(101, 200)) // 101 ~ 999
+
+// 04-04 - 표준 내장 객체 - Math
+console.log('** 04-04 - 표준 내장 객체 - Math **')
