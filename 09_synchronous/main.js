8aeff5cd7b1fb954cf6790e04383618f6d9e840e
ZzzwaHoon <kkjw94@gmail.com>
2024-05-18 16:31:46 +0900

    09-06 - 비동기 - 이행과 거부, 예외 처리 1

--- 09_synchronous/main.js
+++ 09_synchronous/main.js
@@ -227,47 +227,108 @@ document.body.append(ulEl)
 // })
 
 // async & await 사용으로 수정
+// h1El.addEventListener('click', async () => {
+//   // ulEl.textContent = 'Loading...'
+//   const loaderEl = document.createElement('div')
+//   loaderEl.classList.add('loader')
+//   ulEl.innerHTML = ''
+//   ulEl.append(loaderEl)
+//   const res = await fetch('https://api.heropy.dev/v0/users')
+//   const data = await res.json()
+//   console.log(data)
+//   const { users } = data
+//   const liEls = users.map(user => {
+//     const liEl = document.createElement('li')
+//     liEl.textContent = user.name
+//     // const imgEl = document.createElement('img')
+//     // imgEl.src = user.photo?.url || 'https://heropy.dev/favicon.png'
+//     liEl.dataset.photo = user.photo?.url || 'https://heropy.dev/favicon.png'
+//     if (!user.photo) {
+//       liEl.classList.add('no-photo')
+//     }
+//     const loaderEl = document.createElement('div')
+//     loaderEl.classList.add('loader')
+//     liEl.prepend(loaderEl)
+//     return liEl
+//   })
+//   // ulEl.textContent = ''
+//   loaderEl.remove()
+//   ulEl.append(...liEls)
+//   liEls.forEach(async liEl => {
+//     const imgEl = await loadImage(liEl.dataset.photo)
+//     liEl.prepend(imgEl)
+//     liEl.querySelector('.loader').remove()
+//   })
+// })
+
+// function loadImage(src) {
+//   return new Promise (resolve => {
+//     const imgEl = document.createElement('img')
+//     imgEl.src = src
+//     imgEl.addEventListener('load', () => {
+//       resolve(imgEl)
+//     })
+//   })
+// }
+
+// 09-06 - 비동기 - 이행과 거부, 예외 처리 1
+
+// 예외 처리
+console.log('**** 09-06 - 비동기 - 이행과 거부, 예외 처리 1 ****')
 h1El.addEventListener('click', async () => {
   // ulEl.textContent = 'Loading...'
   const loaderEl = document.createElement('div')
   loaderEl.classList.add('loader')
   ulEl.innerHTML = ''
   ulEl.append(loaderEl)
-  const res = await fetch('https://api.heropy.dev/v0/users')
-  const data = await res.json()
-  console.log(data)
-  const { users } = data
-  const liEls = users.map(user => {
-    const liEl = document.createElement('li')
-    liEl.textContent = user.name
-    // const imgEl = document.createElement('img')
-    // imgEl.src = user.photo?.url || 'https://heropy.dev/favicon.png'
-    liEl.dataset.photo = user.photo?.url || 'https://heropy.dev/favicon.png'
-    if (!user.photo) {
-      liEl.classList.add('no-photo')
-    }
-    const loaderEl = document.createElement('div')
-    loaderEl.classList.add('loader')
-    liEl.prepend(loaderEl)
-    return liEl
-  })
-  // ulEl.textContent = ''
-  loaderEl.remove()
-  ulEl.append(...liEls)
-  liEls.forEach(async liEl => {
-    const imgEl = await loadImage(liEl.dataset.photo)
-    liEl.prepend(imgEl)
-    liEl.querySelector('.loader').remove()
-  })
+  try {
+    const res = await fetch('https://api.heropy.dev/v0/users')
+    const data = await res.json()
+    console.log(data)
+    const { users } = data
+    const liEls = users.map(user => {
+      const liEl = document.createElement('li')
+      liEl.textContent = user.name
+      // const imgEl = document.createElement('img')
+      // imgEl.src = user.photo?.url || 'https://heropy.dev/favicon.png'
+      liEl.dataset.photo = user.photo?.url || 'https://heropy.dev/favicon.png'
+      if (!user.photo) {
+        liEl.classList.add('no-photo')
+      }
+      const loaderEl = document.createElement('div')
+      loaderEl.classList.add('loader')
+      liEl.prepend(loaderEl)
+      return liEl
+    })
+    // ulEl.textContent = ''
+    loaderEl.remove()
+    ulEl.append(...liEls)
+    liEls.forEach(async liEl => {
+      try {
+        const imgEl = await loadImage(liEl.dataset.photo)
+        liEl.prepend(imgEl)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        liEl.querySelector('.loader').remove()
+      }
+    })
+  } catch (error) {
+    console.log(error)
+    ulEl.textContent = '사용자 정보를 찾을 수 없어요...'
+    loaderEl.remove()
+  }
 })
 
 function loadImage(src) {
-  return new Promise (reseolve => {
+  return new Promise ((resolve, reject) => {
     const imgEl = document.createElement('img')
     imgEl.src = src
     imgEl.addEventListener('load', () => {
-      reseolve(imgEl)
+      resolve(imgEl)
+    })
+    imgEl.addEventListener('error', () => {
+      reject(new Error('이미지를 로드할 수 없어요...'))
     })
   })
 }
-
